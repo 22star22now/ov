e61452aacb3888578dfb503e80d5f52c9b606665
Star <linus@kottyway.se>
2025-01-22 19:49:55 +0000

    dv

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-function toggleFullscreen(iframeClass) {
-    const iframe = document.querySelector(`.${iframeClass}`);
-    if (iframe.requestFullscreen) {
-        iframe.requestFullscreen();
-    } else if (iframe.mozRequestFullScreen) {
-        iframe.mozRequestFullScreen(); // Firefox
-    } else if (iframe.webkitRequestFullscreen) {
-        iframe.webkitRequestFullscreen(); // Chrome, Safari, Opera
-    } else if (iframe.msRequestFullscreen) {
-        iframe.msRequestFullscreen(); // IE/Edge
+function toggleFullscreen(elementId) {
+    const element = document.getElementById(elementId);
+
+    if (element.requestFullscreen) {
+        element.requestFullscreen(); // Standard method
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen(); // Firefox
+    } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen(); // Chrome, Safari, Opera
+    } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen(); // IE/Edge
+    } else {
+        alert('Fullscreen is not supported by this browser.');
     }
 }
